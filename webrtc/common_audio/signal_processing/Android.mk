c4c8f6b7344e909b092f4c9aad6dfdd302571248
Christian Jacquemot <Christian.Jacquemot@twinlife.org>
2012-04-03 17:35:18 +0200

    Port WebRTC extension to Android
     Modified files:
      o Android.mk
        Add video support
        Remove useless libraries libprotobuf-cpp-2.3.0-lite
        Add missing library to build with Android NDK
      o android-webrtc.mk
        Remove -DWEBRTC_TARGET_PC
      o src/common_audio/signal_processing/Android.mk
        Add LOCAL_ARM_NEON := true required by Android NDK to compile neon code
      o src/common_video/libyuv/Android.mk
        Cleanup of include directive
      o src/modules/audio_device/main/source/audio_device_impl.cc
        Use a global variable to allow multiple calls to CreatePlatformSpecificObjects()
      o src/modules/audio_processing/Android.mk
        Do not build test programs
      o src/modules/rtp_rtcp/source/rtp_format_vp8.cc
        Do not trigger assert() in video encoding. This should be investigated.
      o src/modules/video_capture/main/source/Android/device_info_android.cc
      o src/modules/video_capture/main/source/Android/video_capture_android.cc
      o src/modules/video_capture/main/source/Android/video_capture_android.h
      o src/modules/video_render/main/source/Android/video_render_android_impl.cc
      o src/modules/video_render/main/source/Android/video_render_android_impl.h
      o src/modules/video_render/main/source/Android/video_render_android_native_opengl2.cc
      o src/modules/video_render/main/source/Android/video_render_android_native_opengl2.h
      o src/modules/video_render/main/source/Android/video_render_android_surface_view.cc
      o src/modules/video_render/main/source/Android/video_render_android_surface_view.h
        Cleanup of Android Java upcall support
      o src/modules/video_render/main/source/Android.mk
        Add -DANDROID_NDK_8_OR_ABOVE
      o src/system_wrappers/source/cpu.cc
        Use Linux implementation for Android
      o src/system_wrappers/source/thread_posix.cc
        Implement SetAffinity()
      o src/voice_engine/main/source/output_mixer.cc
        Fix trace message
      o src/voice_engine/main/source/voice_engine_impl.cc
        Allocate only one VoiceEngine object

--- webrtc/common_audio/signal_processing/Android.mk
+++ webrtc/common_audio/signal_processing/Android.mk
@@ -58,7 +58,8 @@ LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/../.. \
     external/webrtc
 
-ifeq ($(ARCH_ARM_HAVE_ARMV7A),true)
+ifeq ($(ARCH_ARM_HAVE_NEON),true)
+LOCAL_ARM_NEON := true
 LOCAL_SRC_FILES += \
     filter_ar_fast_q12_armv7.S
 else
