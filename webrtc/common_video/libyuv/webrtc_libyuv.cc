3477fa7332d7b12669231fd5e6c6d598e1cbc793
Christian Jacquemot <Christian.Jacquemot@twinlife.org>
2012-05-30 17:45:07 +0200

    Update webrtc source code based on google repository (16/05/2012)
     Modified files:
      o src/common_video/libyuv/libyuv.cc
      o src/common_video/libyuv/scaler.cc
        - Fix compilation issues
      o src/modules/audio_processing/Android.mk
        - Remove audio dump support
      o src/modules/rtp_rtcp/source/Android.mk
        - Update source file list
      o src/modules/video_capture/main/source/android/device_info_android.cc
      o src/modules/video_capture/main/source/android/video_capture_android.cc
      o src/modules/video_capture/main/source/android/video_capture_android.h
        - Cleanup Android Java upcall support
      o src/video_engine/Android.mk
      o src/voice_engine/main/source/Android.mk
        - Add SVN version
      o src/voice_engine/main/source/channel.cc
        - Do not generate an error if Channel::SetRecPayloadType() is called on a
        channel that is playing
      o src/voice_engine/main/source/voe_codec_impl.cc
        - Do not generate an error if VoECodecImpl::SetSendCNPayloadType() is
          called on a channel that is recording
    
     New files:
      o src/modules/bitrate_controller/Android.mk
    
     Deleted files:
      o src/modules/video_capture/main/source/Android/device_info_android.cc
      o src/modules/video_capture/main/source/Android/video_capture_android.cc
      o src/modules/video_capture/main/source/Android/video_capture_android.h
        - .../Android has been renamed .../android
      o src/modules/video_render/main/source/Android/video_render_android_impl.cc
      o src/modules/video_render/main/source/Android/video_render_android_native_opengl2.cc
      o src/modules/video_render/main/source/Android/video_render_android_native_opengl2.h
      o src/modules/video_render/main/source/Android/video_render_android_surface_view.cc
      o src/modules/video_render/main/source/Android/video_render_android_surface_view.h
        - .../Android has been renamed .../android

--- webrtc/common_video/libyuv/webrtc_libyuv.cc
+++ webrtc/common_video/libyuv/webrtc_libyuv.cc
@@ -13,7 +13,13 @@
 #include <assert.h>
 #include <string.h>
 
-#include "libyuv.h"
+// LibYuv includes
+// -CJ- 30052012
+#ifdef WEBRTC_ANDROID
+#include "libyuv/files/include/libyuv.h"
+#else
+#include "third_party/libyuv/include/libyuv.h"
+#endif
 
 namespace webrtc {
 
