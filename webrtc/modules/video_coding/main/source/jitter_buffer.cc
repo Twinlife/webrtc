182929cbfef7c44dbfaae900f156da04155f6275
Christian Jacquemot <Christian.Jacquemot@twinlife.org>
2013-06-08 19:04:24 +0200

    Bug #495: Too long processing time of Incoming frame
     Modified file:
      o webrtc/modules/video_coding/main/source/jitter_buffer.c
        Comment out warning message "Jitter buffer drop count"

--- webrtc/modules/video_coding/main/source/jitter_buffer.cc
+++ webrtc/modules/video_coding/main/source/jitter_buffer.cc
@@ -1191,9 +1191,10 @@ bool VCMJitterBuffer::RecycleFramesUntilKeyFrame() {
     }
   }
   drop_count_ += dropped_frames;
-  WEBRTC_TRACE(webrtc::kTraceWarning, webrtc::kTraceVideoCoding,
-               VCMId(vcm_id_, receiver_id_),
-               "Jitter buffer drop count:%u", drop_count_);
+  // --CJ-- 08032013
+  //WEBRTC_TRACE(webrtc::kTraceWarning, webrtc::kTraceVideoCoding,
+  //             VCMId(vcm_id_, receiver_id_),
+  //             "Jitter buffer drop count:%u", drop_count_);
   TRACE_EVENT_INSTANT0("webrtc", "JB::RecycleFramesUntilKeyFrame");
   if (key_frame_found) {
     // Reset last decoded state to make sure the next frame decoded is a key
