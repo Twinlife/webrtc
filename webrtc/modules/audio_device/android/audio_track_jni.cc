62f3d8aae1a86e1699554dedf1ff81e06db97a90
Christian Jacquemot <Christian.Jacquemot@twinlife-systems.com>
2016-11-18 13:38:43 +0100

    Bug #1688: Synchronization with Google repository (WebRTC 56)
     Modified files:
      o webrtc/modules/audio_device/android/audio_manager.cc
      o webrtc/modules/audio_device/android/audio_record_jni.cc
      o webrtc/modules/audio_device/android/audio_track_jni.cc
      o webrtc/modules/utility/source/jvm_android.cc
        Reduce debug log information

--- webrtc/modules/audio_device/android/audio_track_jni.cc
+++ webrtc/modules/audio_device/android/audio_track_jni.cc
@@ -20,8 +20,10 @@
 #include "webrtc/base/format_macros.h"
 
 #define TAG "AudioTrackJni"
-#define ALOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, TAG, __VA_ARGS__)
-#define ALOGD(...) __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
+// -twinlife- 160719
+#define ALOGV(...) //__android_log_print(ANDROID_LOG_VERBOSE, TAG, __VA_ARGS__)
+#define ALOGD(...) //__android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
+// -twinlife- 160719
 #define ALOGE(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
 #define ALOGW(...) __android_log_print(ANDROID_LOG_WARN, TAG, __VA_ARGS__)
 #define ALOGI(...) __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
--- webrtc/modules/utility/source/jvm_android.cc
+++ webrtc/modules/utility/source/jvm_android.cc
@@ -17,7 +17,9 @@
 #include "webrtc/base/checks.h"
 
 #define TAG "JVM"
-#define ALOGD(...) __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
+// -twinlife- 160719
+#define ALOGD(...) //__android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
+// -twinlife- 160719
 #define ALOGE(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
 
 namespace webrtc {
