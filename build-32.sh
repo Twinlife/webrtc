cf170af88cbbda47ebb3e46fdf75effcbbb1385a
Christian Jacquemot <Christian.Jacquemot@twinlife-systems.com>
2017-11-15 18:05:56 +0100

    Bug #1872: Synchronization with Google repository (WebRTC 63)
     Modified files:
      o build-32.sh
      o build-64.sh
        Set use_errorprone_java_compiler to false

--- build-32.sh
+++ build-32.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm -rf out-32/Release
-buildtools/linux64/gn gen out-32/Release --args='target_os="android" target_cpu="arm" is_official_build=true rtc_enable_protobuf=false rtc_include_tests=false'
+buildtools/linux64/gn gen out-32/Release --args='target_os="android" target_cpu="arm" is_official_build=true rtc_enable_protobuf=false rtc_include_tests=false use_errorprone_java_compiler=false'
 
 
