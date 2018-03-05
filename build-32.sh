9e4524758ef9f5384a365dca584ab5e4f80cbd21
Christian Jacquemot <Christian.Jacquemot@twinlife-systems.com>
2018-03-05 15:44:41 +0100

    Bug #1916: Build on MacOS
     Modified files:
      o build-32.sh
      o build-64.sh
        Build on Linux and MacOS

--- build-32.sh
+++ build-32.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 rm -rf out-32/Release
-buildtools/linux64/gn gen out-32/Release --args='target_os="android" target_cpu="arm" is_official_build=true rtc_enable_protobuf=false rtc_include_tests=false use_errorprone_java_compiler=false'
+os=`uname`
+if [[ $os == 'Linux' ]];
+then
+    gn=buildtools/linux64/gn
+elif [[ $os == 'Darwin' ]];
+then
+    gn=buildtools/mac/gn
+fi
+$gn gen out-32/Release --args='target_os="android" target_cpu="arm" is_official_build=true rtc_enable_protobuf=false rtc_include_tests=false use_errorprone_java_compiler=false'
 
 
