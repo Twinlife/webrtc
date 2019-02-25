ded82a821bf78f36a141664f5a0f8129b74d6f39
Christian Jacquemot <Christian.Jacquemot@twinlife-systems.com>
2019-02-25 09:43:09 +0100

    Bug #2415: Application crash in video encoder wrapper after wrapper release
     Modified files:
      o sdk/android/src/jni/videoencoderwrapper.cc
        In VideoEncoderWrapper::OnEncodedFrame() check encoder_queue_

--- sdk/android/src/jni/videoencoderwrapper.cc
+++ sdk/android/src/jni/videoencoderwrapper.cc
@@ -282,10 +282,14 @@ void VideoEncoderWrapper::OnEncodedFrame(JNIEnv* jni,
     }
   };
 
-  encoder_queue_->PostTask(
-      Lambda{this, std::move(buffer_copy), qp, encoded_width, encoded_height,
-             capture_time_ns, frame_type, rotation, complete_frame,
-             &frame_extra_infos_, callback_});
+  // -twinlife- 190225
+  if (encoder_queue_) {
+    encoder_queue_->PostTask(
+        Lambda{this, std::move(buffer_copy), qp, encoded_width, encoded_height,
+               capture_time_ns, frame_type, rotation, complete_frame,
+               &frame_extra_infos_, callback_});
+  }
+  // -twinlife- 190225
 }
 
 int32_t VideoEncoderWrapper::HandleReturnCode(JNIEnv* jni,
