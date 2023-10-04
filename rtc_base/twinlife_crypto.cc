9e4366093ae9ebb581273ab5b7603872d6d38524
Stephane Carrez <Stephane.Carrez@skyrock.com>
2023-10-04 10:20:44 +0200

    Feature #16: Support pour chiffrer et signer les SDPs et attributs des twincodes
      Modified files:
        o rtc_base/BUILD.gn
          Build the new files.
    
      New files:
        o rtc_base/twinlife_crypto.cc
        o rtc_base/twinlife_crypto.h
          New Twinlife::Crypto class with operations to generate a private/public key,
          sign with ECDSA, verify the signature, encrypt and decrypt with AEAD.
          (tested on Linux and verified with valgrind)

---
+++ rtc_base/twinlife_crypto.cc
@@ -0,0 +1,349 @@
+/*
+ *  Copyright (c) 2023 twinlife SA.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor:
+ *   Stephane Carrez (Stephane.Carrez@skyrock.com)
+ */
+#include "twinlife_crypto.h"
+#include <openssl/conf.h>
+#include <openssl/evp.h>
+#include <openssl/err.h>
+#include <openssl/bio.h>
+#include <openssl/bn.h>
+#include <openssl/ec.h>
+#include <openssl/ecdsa.h>
+#include <openssl/bytestring.h>
+#include <openssl/mem.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+#define nullptr 0
+#define NAMESPACE namespace twinlife {
+
+NAMESPACE
+
+int Crypto::digest(const unsigned char* data, int len, unsigned char digest[EVP_MAX_MD_SIZE])
+{
+  unsigned int digest_len = EVP_MAX_MD_SIZE;
+  EVP_MD_CTX* ctx = EVP_MD_CTX_new();
+  const EVP_MD* md = EVP_sha256();
+
+  EVP_MD_CTX_init(ctx);
+  EVP_DigestInit_ex(ctx, md, 0);
+  EVP_DigestUpdate(ctx, data, len);
+  EVP_DigestFinal(ctx, digest, &digest_len);
+  EVP_MD_CTX_destroy(ctx);
+  return digest_len;
+}
+
+Crypto* Crypto::create()
+{
+  EC_KEY* ec_key = EC_KEY_new_by_curve_name(NID_X9_62_prime256v1);
+  if (!ec_key) {
+    return nullptr;
+  }
+
+  EVP_PKEY* pkey = EVP_PKEY_new();
+  if (!pkey || !EC_KEY_generate_key(ec_key) || !EVP_PKEY_assign_EC_KEY(pkey, ec_key)) {
+    EVP_PKEY_free(pkey);
+    EC_KEY_free(ec_key);
+    return nullptr;
+  }
+
+  return new Crypto(pkey);
+}
+
+Crypto* Crypto::importPublicKey(const unsigned char* pubKey, size_t pubKeyLength)
+{
+  unsigned char buffer[TWINLIFE_MAX_PUBKEY_LENGTH];
+  size_t length;
+
+  if (!pubKey || pubKeyLength <= 0 || pubKeyLength > TWINLIFE_MAX_SIZE) {
+    return nullptr;
+  }
+  if (EVP_DecodeBase64(buffer, &length, sizeof(buffer), pubKey, pubKeyLength) != 1) {
+    return nullptr;
+  }
+
+  CBS cbs;
+  CBS_init(&cbs, buffer, length);
+
+  EVP_PKEY* pkey = EVP_parse_public_key(&cbs);
+  if (!pkey) {
+    return nullptr;
+  }
+
+  if (CBS_len(&cbs) != 0) {
+    EVP_PKEY_free(pkey);
+    return nullptr;
+  }
+
+  return new Crypto(pkey);
+}
+
+Crypto* Crypto::importPrivateKey(const unsigned char* privateKey, size_t privateKeyLength)
+{
+  if (!privateKey || privateKeyLength <= 0 || privateKeyLength > TWINLIFE_MAX_SIZE) {
+    return nullptr;
+  }
+
+  CBS cbs;
+  CBS_init(&cbs, privateKey, privateKeyLength);
+
+  EVP_PKEY* pkey = EVP_parse_private_key(&cbs);
+  if (!pkey) {
+    return nullptr;
+  }
+
+  if (CBS_len(&cbs) != 0) {
+    EVP_PKEY_free(pkey);
+    return nullptr;
+  }
+
+  return new Crypto(pkey);
+}
+
+int Crypto::exportPublicKey(unsigned char* buffer, size_t maxLength)
+{
+  CBB cbb;
+  uint8_t *data;
+  size_t size;
+
+  if (!buffer || maxLength <= 0) {
+    return TWINLIFE_BAD_PARAM;
+  }
+
+  CBB_init(&cbb, TWINLIFE_MAX_SIZE);
+  if (EVP_marshal_public_key(&cbb, pkey_) != 1) {
+    CBB_cleanup(&cbb);
+    return TWINLIFE_TOO_SMALL;
+  }
+  CBB_finish(&cbb, &data, &size);
+
+  // Verify we have enough space for BASE64 (+5 is for /3 rounding + 1 for NUL).
+  if (4 * (size / 3) + 5 >= maxLength) {
+    OPENSSL_free(data);
+    return TWINLIFE_TOO_SMALL;
+  }
+
+  int result = EVP_EncodeBlock(buffer, data, size);
+  OPENSSL_free(data);
+  return result;
+}
+
+int Crypto::exportPrivateKey(unsigned char* buffer, size_t maxLength)
+{
+  CBB cbb;
+  uint8_t *data;
+  size_t size;
+
+  if (!buffer || maxLength <= 0) {
+    return TWINLIFE_BAD_PARAM;
+  }
+  CBB_init_fixed(&cbb, buffer, maxLength);
+  if (EVP_marshal_private_key(&cbb, pkey_) != 1) {
+    return TWINLIFE_TOO_SMALL;
+  }
+  CBB_finish(&cbb, &data, &size);
+
+  return size;
+}
+
+int Crypto::signECDSA(const unsigned char* data, size_t len, unsigned char* signature, size_t maxLength)
+{
+  if (!data || !signature || len <= 0 || maxLength <= 0) {
+    return TWINLIFE_BAD_PARAM;
+  }
+
+  unsigned char digest[EVP_MAX_MD_SIZE];
+  memset(digest, 0, sizeof(digest));
+  int digest_len = Crypto::digest(data, len, digest);
+  if (digest_len <= 0) {
+    return TWINLIFE_BAD_ALLOC;
+  }
+
+  const EC_KEY *ec_key = EVP_PKEY_get0_EC_KEY(pkey_);
+  if (!ec_key) {
+    return TWINLIFE_BAD_EC_KEY;
+  }
+
+  ECDSA_SIG *sig = ECDSA_do_sign(digest, digest_len, (EC_KEY *)ec_key);
+  if (!sig) {
+    return TWINLIFE_SIGN_ERROR;
+  }
+
+  const BIGNUM *r = ECDSA_SIG_get0_r(sig);
+  const BIGNUM *s = ECDSA_SIG_get0_s(sig);
+  if (!r || !s) {
+    ECDSA_SIG_free(sig);
+    return TWINLIFE_BAD_SIGNATURE;
+  }
+
+  int rlen = BN_num_bytes(r);
+  int slen = BN_num_bytes(s);
+  int sigLength = slen + rlen;
+  if (rlen <= 0 || slen <= 0 || sigLength >= TWINLIFE_MAX_SIGLEN) {
+    ECDSA_SIG_free(sig);
+    return TWINLIFE_BAD_SIGNATURE;
+  }
+
+  unsigned char buf[TWINLIFE_MAX_SIGLEN];
+  rlen = BN_bn2bin(r, buf);
+  slen = BN_bn2bin(s, &buf[rlen]);
+
+  ECDSA_SIG_free(sig);
+
+  // Verify we have enough space for BASE64 (+5 is for /3 rounding + 1 for NUL).
+  if ((size_t) (4 * (sigLength / 3) + 5) >= maxLength) {
+    return TWINLIFE_TOO_SMALL;
+  }
+
+  size_t result = EVP_EncodeBlock(signature, buf, sigLength);
+  return result;
+}
+
+int Crypto::verifyECDSA(const unsigned char* data, size_t len, unsigned char* signature, size_t signatureLength)
+{
+  if (!data || !signature || len <= 0 || signatureLength <= 0) {
+    return TWINLIFE_BAD_PARAM;
+  }
+
+  unsigned char digest[EVP_MAX_MD_SIZE];
+  int digest_len = Crypto::digest(data, len, digest);
+  if (digest_len <= 0) {
+    return TWINLIFE_BAD_ALLOC;
+  }
+
+  const EC_KEY *ec_key = EVP_PKEY_get0_EC_KEY(pkey_);
+  if (!ec_key) {
+    return TWINLIFE_BAD_EC_KEY;
+  }
+
+  unsigned char buffer[TWINLIFE_MAX_SIGLEN];
+  size_t length;
+  if (EVP_DecodeBase64(buffer, &length, sizeof(buffer), signature, signatureLength) != 1) {
+    return TWINLIFE_BAD_SIGNATURE;
+  }
+
+  int slen = length / 2;
+  BIGNUM *r = BN_bin2bn(buffer, slen, 0);
+  BIGNUM *s = BN_bin2bn(&buffer[slen], length - slen, 0);
+  ECDSA_SIG *sig = ECDSA_SIG_new();
+
+  ECDSA_SIG_set0(sig, r, s);
+  int result = ECDSA_do_verify(digest, digest_len, sig, (EC_KEY *)ec_key);
+  ECDSA_SIG_free(sig);
+
+  return result;
+}
+
+int Crypto::createSharedSecret(const Crypto* peerPublicKey, unsigned char* key, size_t keyLength)
+{
+  if (!peerPublicKey || !key || keyLength <= 0) {
+    return TWINLIFE_BAD_PARAM;
+  }
+
+  EVP_PKEY_CTX *keyCtx = EVP_PKEY_CTX_new(pkey_, nullptr);
+  if (!keyCtx) {
+    return TWINLIFE_BAD_ALLOC;
+  }
+
+  if (EVP_PKEY_derive_init(keyCtx) <= 0) {
+    EVP_PKEY_CTX_free(keyCtx);
+    return TWINLIFE_BAD_ALLOC;
+  }
+
+  if (EVP_PKEY_derive_set_peer(keyCtx, peerPublicKey->pkey_) <= 0) {
+    EVP_PKEY_CTX_free(keyCtx);
+    return TWINLIFE_BAD_PARAM;
+  }
+
+  if (EVP_PKEY_derive(keyCtx, key, &keyLength) <= 0) {
+    EVP_PKEY_CTX_free(keyCtx);
+    return TWINLIFE_BAD_PARAM;
+  }
+  EVP_PKEY_CTX_free(keyCtx);
+
+  return Crypto::digest(key, keyLength, key);
+}
+
+int Crypto::bind(const Crypto *peerPublicKey, unsigned char nonce[TWINLIFE_NONCE_LENGTH], int maxIncrement) {
+
+  unbind();
+  unsigned char* key = (unsigned char*)OPENSSL_malloc(TWINLIFE_MAX_SECRET_SIZE);
+  if (!key) {
+    return TWINLIFE_BAD_ALLOC;
+  }
+
+  int keyLength = createSharedSecret(peerPublicKey, key, TWINLIFE_MAX_SECRET_SIZE);
+  if (keyLength <= 0) {
+    OPENSSL_free(key);
+    return TWINLIFE_BAD_PARAM;
+  }
+  newNonce(nonce, maxIncrement);
+
+  const EVP_AEAD *aead = EVP_aead_aes_256_gcm();
+
+  aead_ = EVP_AEAD_CTX_new(aead, key, keyLength, 0);
+  OPENSSL_free(key);
+  return aead_ ? 1 : TWINLIFE_BAD_ALLOC;
+}
+
+void Crypto::unbind() {
+  if (aead_) {
+    EVP_AEAD_CTX_free(aead_);
+    aead_ = nullptr;
+  }
+}
+
+void Crypto::newNonce(unsigned char nonce[TWINLIFE_NONCE_LENGTH], int maxIncrement)
+{
+  memcpy(nonce_, nonce, TWINLIFE_NONCE_LENGTH);
+  maxIncrement_ = maxIncrement;
+}
+
+int Crypto::encryptAEAD(const unsigned char* data, size_t len, const unsigned char* auth, size_t authLength,
+                        unsigned char* nonce, unsigned char* buffer, size_t maxLength)
+{
+  if (!aead_) {
+    return TWINLIFE_BAD_PARAM;
+  }
+
+  int v = std::atomic_fetch_add<int>(&nonceVal_, 1);
+  if (v >= maxIncrement_) {
+    return TWINLIFE_NONCE_ERROR;
+  }
+  memcpy(nonce, nonce_, sizeof(nonce_));
+  nonce[TWINLIFE_NONCE_LENGTH - 1] = v;
+
+  size_t outLength;
+  int result = EVP_AEAD_CTX_seal(aead_, buffer, &outLength, maxLength, nonce, sizeof(nonce_), data, len, auth, authLength);
+  return result <= 0 ? TWINLIFE_AEAD_FAIL : outLength;
+}
+
+int Crypto::decryptAEAD(const unsigned char* encryptedData, size_t len,
+                        const unsigned char* auth, size_t authLength,
+                        const unsigned char* nonce, unsigned char* buffer, size_t maxLength)
+{
+  if (!aead_) {
+    return TWINLIFE_BAD_PARAM;
+  }
+
+  size_t outLength;
+  int result = EVP_AEAD_CTX_open(aead_, buffer, &outLength, maxLength, nonce, sizeof(nonce_), encryptedData, len, auth, authLength);
+  return result <= 0 ? TWINLIFE_AEAD_FAIL : outLength;
+}
+
+Crypto::~Crypto()
+{
+   EVP_PKEY_free(pkey_);
+   if (aead_) {
+     EVP_AEAD_CTX_free(aead_);
+   }
+}
+
+}
---
+++ rtc_base/twinlife_crypto.h
@@ -0,0 +1,109 @@
+/*
+ *  Copyright (c) 2023 twinlife SA.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributor:
+ *   Stephane Carrez (Stephane.Carrez@skyrock.com)
+ */
+#ifndef TWINLIFE_SIGN_H
+#define TWINLIFE_SIGN_H
+
+#include <openssl/evp.h>
+#include <atomic>
+
+#define TWINLIFE_MAX_SIGLEN        128 // 64-bytes in real
+#define TWINLIFE_MAX_PUBKEY_LENGTH 128 // 91-bytes in real
+#define TWINLIFE_MAX_SIZE          256 // Max size for buffers (enough for pubkey in BASE64)
+#define TWINLIFE_MAX_SECRET_SIZE   32  // ECDH shared keys are 256-bits == 32-bytes
+#define TWINLIFE_NONCE_LENGTH      12  // Length of the nonce
+
+#define TWINLIFE_BAD_PARAM     (-1)
+#define TWINLIFE_TOO_SMALL     (-2)
+#define TWINLIFE_BAD_ALLOC     (-3)
+#define TWINLIFE_BAD_EC_KEY    (-4)
+#define TWINLIFE_SIGN_ERROR    (-5)
+#define TWINLIFE_BAD_SIGNATURE (-6)
+#define TWINLIFE_AEAD_FAIL     (-7)
+#define TWINLIFE_NONCE_ERROR   (-8)
+
+namespace twinlife {
+  class Crypto {
+  public:
+    // Create a private/public keypair for ECDSA (use prime256v1 EC).
+    static Crypto* create();
+
+    // Create the instance by importing a DER BASE64 public key.  Returns null if the format is invalid.
+    static Crypto* importPublicKey(const unsigned char* pubKey, size_t pubKeyLength);
+
+    // Create the instance by importing a DER binary private key.  Returns null if the format is invalid.
+    static Crypto* importPrivateKey(const unsigned char* privateKey, size_t privateKeyLength);
+
+    // Export the public key in DER base64 in the given buffer and return the length of exported public key.
+    int exportPublicKey(unsigned char* buffer, size_t maxLength);
+
+    // Export the priviate key in DER in the given buffer (no base64 encoding)
+    // and return the length of exported private key.
+    int exportPrivateKey(unsigned char* buffer, size_t maxLength);
+
+    // Sign the content of the data buffer with the private key and encode the ECDSA signature in Base64
+    // in the signature buffer.  Return the length of the signature or a negative error code.
+    int signECDSA(const unsigned char* data, size_t len, unsigned char* signature, size_t maxLength);
+
+    // Verify with the public key that the data buffer corresponds to the Base64 ECDSA signature.
+    // Returns 1 if the signature is verified, 0 if the data does not match or a negative error code.
+    int verifyECDSA(const unsigned char* data, size_t len, unsigned char* signature, size_t signatureLength);
+
+    // Prepare for use of AEAD with the peer's public key.  Derive a shared secret based on the private key
+    // and peer's public key, compute the SHA256 digest of that secret, setup the AEAD internal context
+    // to be ready to use `encryptAEAD` or `decryptAEAD`.  The `bind` is a costly operation compared
+    // to encryption and decryption.  The encryption nonce is pre-initialized with the given buffer
+    // and will be incremented before each encryptAEAD() a maximum of `maxIncrement` times.
+    int bind(const Crypto *peerPublicKey, unsigned char nonce[TWINLIFE_NONCE_LENGTH], int maxIncrement);
+
+    // Unbind with peer's public key and release the AEAD context.  This operation must be called when
+    // encryption and decryption are not necessary any more.
+    void unbind();
+
+    // Setup a new nonce for encryptAEAD().
+    void newNonce(unsigned char nonce[TWINLIFE_NONCE_LENGTH], int maxIncrement);
+
+    // Encrypt and sign with AES256-GCM the data buffer and auth buffer with a new nonce.
+    // Only the data buffer is encrypted.  The nonce buffer will be filled with a new nonce of 12 bytes.
+    // Return the length of the output buffer or a negative error code.
+    int encryptAEAD(const unsigned char* data, size_t len,
+                    const unsigned char* auth, size_t auth_length, unsigned char* nonce,
+                    unsigned char* buffer, size_t maxLength);
+
+    // Decrypt and verify the data with AES256-GCM.  Only the encryptedData buffer is decrypted.
+    int decryptAEAD(const unsigned char* encryptedData, size_t len,
+                    const unsigned char *auth, size_t auth_length, const unsigned char* nonce,
+                    unsigned char* buffer, size_t maxLength);
+
+    Crypto(EVP_PKEY *pkey) {
+      pkey_ = pkey;
+      aead_ = nullptr;
+    }
+    ~Crypto();
+
+    // Forbid copy and assignment.
+    Crypto(const Crypto&) = delete;
+    Crypto& operator=(const Crypto&) = delete;
+
+  private:
+    EVP_PKEY* pkey_;
+    EVP_AEAD_CTX *aead_;
+    unsigned char nonce_[TWINLIFE_NONCE_LENGTH];
+    int maxIncrement_;
+    std::atomic<int> nonceVal_;
+
+    static int digest(const unsigned char* data, int len, unsigned char digest[EVP_MAX_MD_SIZE]);
+
+    // Derive a shared secret based on the private key and peer's public key, compute the SHA256 digest of that
+    // secret and return it in the `key` buffer.  The `key` buffer must be allocated by using OPENSSL_malloc()
+    // for security constraints (key will be cleared when buffer is released).
+    int createSharedSecret(const Crypto* peerPublicKey, unsigned char* key, size_t keyLength);
+  };
+}
+
+#endif
