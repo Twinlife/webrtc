1f9d8bbbcad5d66bc8cd574f961081f6479f00cc
Stephane Carrez <Stephane.Carrez@skyrock.com>
2024-05-13 17:26:55 +0200

    Feature #19: Intégration WebRTC 125
      Modified files:
        o Update components for WebRTC 125
        o twinlife/webrtc-update.sh
          Update script to simplify and help the update process

--- twinlife/webrtc-update.sh
+++ twinlife/webrtc-update.sh
@@ -1,28 +1,66 @@
 #!/bin/sh
-WEBRTC_DST_DIR=/build/webrtc-library
-WEBRTC_SRC_DIR=/ext/webrtc/src
+if test T$WEBRTC_DST_DIR = T; then
+   echo "Missing WEBRTC_DST_DIR environment variable"
+   exit 1;
+fi
+if ! test -d "$WEBRTC_DST_DIR"; then
+   echo "WEBRTC_DST_DIR is not a directory"
+   exit 1;
+fi
+if test T$WEBRTC_SRC_DIR = T; then
+   echo "Missing WEBRTC_SRC_DIR environment variable"
+   exit 1;
+fi
+if ! test -d "$WEBRTC_SRC_DIR"; then
+   echo "WEBRTC_SRC_DIR is not a directory"
+   exit 1;
+fi
 
-if test $# -ne 1; then
-   echo "Usage: webrtc-update.sh name"
+if test $# -lt 1; then
+   echo "Usage: webrtc-update.sh name [commit-message]"
    echo "Valid names:"
    (cd $WEBRTC_DST_DIR/third_party && ls)
    exit 2
 fi
 NAME=$1
 
-if test ! -d $WEBRTC_DST_DIR/third_party/$NAME; then
+SRC_DIR="?"
+DST_DIR="?"
+case $NAME in
+    build|base|testing|tools/grit|tools/clang|tools/android)
+        SRC_DIR=$WEBRTC_SRC_DIR/$NAME
+        DST_DIR=$WEBRTC_DST_DIR/$NAME
+        ;;
+
+    *)
+        SRC_DIR=$WEBRTC_SRC_DIR/third_party/$NAME
+        DST_DIR=$WEBRTC_DST_DIR/third_party/$NAME
+        ;;
+esac
+
+if test ! -d $DST_DIR; then
    echo "Invalid $NAME: destination is missing"
    exit 1
 fi
 
-if test ! -d $WEBRTC_SRC_DIR/third_party/$NAME; then
+if test ! -d $SRC_DIR; then
    echo "Invalid $NAME: source is missing"
    exit 1
 fi
 
-cd $WEBRTC_DST_DIR/third_party/$NAME || exit 1
+cd $DST_DIR || exit 1
+git checkout google/master
+if test $? -ne 0; then
+   echo "Switching to google/master branch in $NAME failed"
+   exit 1
+fi
 rm -rf *
-cp -r $WEBRTC_SRC_DIR/third_party/$NAME/* .
+cp -r $SRC_DIR/* .
 git add .
 git status
+if test $# -eq 2; then
+   MESSAGE=$2
+   git commit -m "$MESSAGE"
+   git push
+fi
 
