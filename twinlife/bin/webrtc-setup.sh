bf701d04039048d4018de914b3ecee91f358a10c
Stephane Carrez <Stephane.Carrez@skyrock.com>
2026-02-11 13:04:28 +0100

    Allow us to override (almost) easily some third_party components
    We sometimes need to fix or adapt third_party components and managing
    these override using git or quilt appears more complex than necessary.
    What we override is very limited and we try to avoid that.
    
    - the webrtc-setup.sh script will create the third_party directory
      and create a symbolic link for each file provided by the official
      third_party source
    - for some limited directories, we do a copy instead of a symbolic link
      and then we override some files by copying our own files from
      'twinlife/<third_party-dir>/<file>' and installing them after
      the copy.
    
    For WebRTC 144, the third_party/jni_zero/jni_zero.cc file was modified
    to avoid an unecessary dependency to some stupid jni_zero Java files
    that we don't want.  The jni_zero.cc was modified to disable that.

--- twinlife/bin/webrtc-setup.sh
+++ twinlife/bin/webrtc-setup.sh
@@ -16,9 +16,28 @@ if test ! -f twinlife/bin/webrtc-setup.sh; then
 fi
 
 rm -rf third_party tools base buildtools testing build
-ln -s ${WEBRTC_SRC_DIR}/third_party
 ln -s ${WEBRTC_SRC_DIR}/tools
-# ln -s ${WEBRTC_SRC_DIR}/base
 ln -s ${WEBRTC_SRC_DIR}/buildtools
 ln -s ${WEBRTC_SRC_DIR}/testing
 ln -s ${WEBRTC_SRC_DIR}/build
+
+# Create symbolic link to each third_party directory
+# For some of these third_party, we may do fixes and adaptations.
+# For these limited cases, we copy the WebRTC official directory and override it
+# with our own adaptations that we save in twinlife/<override> directory.
+# It is not perfect but enough and easier to do it that way.
+COPY_THIRD_PARTY_LIBS="jni_zero"
+
+mkdir third_party && (cd third_party &&
+  for i in ${WEBRTC_SRC_DIR}/third_party/*; do
+     ln -s $i
+  done
+  for i in ${COPY_THIRD_PARTY_LIBS}; do
+     rm -rf $i
+     cp -r ${WEBRTC_SRC_DIR}/third_party/$i $i
+  done        
+)
+
+# Apply our own override:
+cp twinlife/jni_zero/jni_zero.cc third_party/jni_zero/
+
